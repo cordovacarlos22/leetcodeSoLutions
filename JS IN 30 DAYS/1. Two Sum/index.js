43acd57a4b5631c6c5b05279c009e7994d2651fe
carloscordova.dev <cordovamanuel22@icloud.com>
2024-04-03 18:07:45 -0700

    add 1. Two sum solution

---
+++ JS IN 30 DAYS/1. Two Sum/index.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] == target) {
+        nums.splice(0);
+        nums.push(i);
+        nums.push(j);
+        return nums
+      }
+    }
+  }
+
+}
